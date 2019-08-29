e91aa992dab2ad9b5ed33960b265576be740bf8a
fullset <fullset@gmail.com>
2019-08-29 12:57:20 +0300

    Add ranges to axis.

--- PaintPlease.py
+++ PaintPlease.py
@@ -3,16 +3,38 @@ import math
 #
 # parser = argparse.ArgumentParser(description='Paint graph of some functions')
 # parser.add_argument()
-
+x_min = -100
+x_max = 100
+y_min = -100
+y_max = 100
 half_height = 10
 half_width = 40
 
 print(' ' * half_width + '^')
 for i in range(half_height):
-    print(' ' * half_width + '|')
+    if i == (half_height / 2):
+        print(' ' * (half_width - 4) + str(y_max / 2) + '|')
+    elif i == 0:
+        print(' ' * (half_width - 3) + str(y_max) + '|')
+    else:
+        print(' ' * half_width + '|')
 print('-' * half_width + '|' + '-' * half_width + '>')
-print(' ' * (half_width - 1) + '0|')
+x_axis = str(x_min)
+x_axis += ' ' * int((half_width - 10) / 2)
+x_axis += str(x_min / 2)
+x_axis += ' ' * int((half_width - 10) / 2)
+x_axis += '0|'
+x_axis += ' ' * int((half_width - 8) / 2)
+x_axis += str(x_max / 2)
+x_axis += ' ' * int((half_width - 8) / 2)
+x_axis += str(x_max)
+print(x_axis)
 for i in range(half_height):
-    print(' ' * half_width + '|')
+    if i == (half_height / 2 - 1):
+        print(' ' * (half_width - 5) + str(y_min / 2) + '|')
+    elif i == half_height - 1:
+        print(' ' * (half_width - 4) + str(y_min) + '|')
+    else:
+        print(' ' * half_width + '|')
 
 
